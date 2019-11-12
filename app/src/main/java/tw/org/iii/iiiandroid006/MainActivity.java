4bb8ff27b7da7607bb0a1f86b7bbc8478a86a04a
Seraphim <freedom1040425@gmail.com>
2019-11-12 13:53:24 +0800

    iiiAndroid006

--- app/src/main/java/tw/org/iii/iiiandroid006/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid006/MainActivity.java
@@ -3,6 +3,7 @@ package tw.org.iii.iiiandroid006;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import java.sql.Time;
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private TextView clock;
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private Boolean isRunning;
     private Button btnright,btnleft;
     private Timer timer;
+    private int i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,14 +33,27 @@ public class MainActivity extends AppCompatActivity {
         btnleft.setText(isRunning?"lap":"Reset");
         btnright.setText(isRunning?"Stop":"Start");
 
+        Log.v("brad","time");
         timer= new Timer();
+        //timer.schedule(new MyTask(),0,10);
 
     }
+    private class MyTask extends TimerTask{
+        @Override
+        public void run(){
+            i++;
+            Log.v("brad","i==>"+i);
+            clock.setText(""+i);
+
+        }
+
+    }
+
     public void clickleft(View view){
         //測試練習 看不到但在背景中執行
         //電腦背景要看前景的臉色過活
         //安卓 不一定
-        
+
 
 
 
@@ -46,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
         isRunning =!isRunning;
         btnleft.setText(isRunning?"lap":"Reset");
         btnright.setText(isRunning?"Stop":"Start");
+
+        timer.schedule(new MyTask(),0,10);
 
     }
     //小明媽媽炒菜，沒有醬油~叫小明(去撈資料)出去買，媽媽繼續炒菜
