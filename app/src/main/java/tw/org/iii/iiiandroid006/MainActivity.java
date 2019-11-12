e1db3f328391719767feb9db8a17a53ce7005b55
Seraphim <freedom1040425@gmail.com>
2019-11-12 11:37:19 +0800

    iiiAndroid006

---
+++ app/src/main/java/tw/org/iii/iiiandroid006/MainActivity.java
@@ -0,0 +1,21 @@
+package tw.org.iii.iiiandroid006;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clickleft(View view){
+
+    }
+
+    public void clickright(View view) {
+    }
+}
